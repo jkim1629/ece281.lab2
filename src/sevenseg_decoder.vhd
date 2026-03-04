00143c0d4622118e5da1daf4cb026baea59f1b7c
Kim <c28joseph.kim@afacademy.af.edu>
2026-03-03 21:48:53 -0700

    Complete Lab 2 Code

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -32,13 +32,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+    port( 
+    i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
+signal seg_active_high : std_logic_vector(6 downto 0);
 begin
-
+with i_Hex select
+  seg_active_high <=
+    "0111111" when "0000",  -- 0
+    "0000110" when "0001",  -- 1
+    "1011011" when "0010",  -- 2
+    "1001111" when "0011",  -- 3
+    "1100110" when "0100",  -- 4
+    "1101101" when "0101",  -- 5
+    "1111101" when "0110",  -- 6
+    "0000111" when "0111",  -- 7
+    "1111111" when "1000",  -- 8
+    "1100111" when "1001",  -- 9
+    "1110111" when "1010",  -- A
+    "1111100" when "1011",  -- b
+    "0111001" when "1100",  -- C
+    "1011110" when "1101",  -- d
+    "1111001" when "1110",  -- E
+    "1110001" when "1111",  -- F
+    "0000000" when others;  -- blank
+            
+           o_seg_n <= not seg_active_high;
 
 end Behavioral;
